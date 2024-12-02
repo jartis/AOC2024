568954152620869fda1e22b3d2d4f7d7a7b72671
J Artis <jartis@users.noreply.github.com>
2024-12-02 17:25:04 -0500

    Brute-force 2-2

--- Day2/index.js
+++ Day2/index.js
@@ -7,26 +7,44 @@ for (let lineIdx = 0; lineIdx < SrcData.length; lineIdx += 1) {
   // 7 6 4 2 1
   const line = SrcData[lineIdx];
 
+  // Make an array of the original, and with each individual element removed
+  const LineArray = [];
+
   // Each line is an array of space-separated ints.
   const values = line.split(' ');
   values.forEach((val) => {
     val = parseInt(val, 10);
   });
-  const signs = [];
-  const diffs = [];
-  for (let valIdx = 0; valIdx < values.length - 1; valIdx += 1) {
-    const actualDiff = values[valIdx] - values[valIdx + 1];
-    const sign = Math.sign(actualDiff);
-    if (valIdx > 0 && sign != signs[0]) continue; // Changing signs!
-    signs.unshift(sign);
-    const posDif = Math.abs(actualDiff);
-    if (posDif < 1 || posDif > 3) continue; // First diff out of range?
-    diffs.unshift(posDif);
+
+  // Now that it's all values, build our arrays!
+  for (let i = 0; i < values.length; i++) {
+    const splicedArr = values.slice();
+    splicedArr.splice(i, 1);
+    LineArray.push(splicedArr);
   }
-  // We made it through the checks - this one is safe!
-  if (diffs.length === values.length - 1) {
-    SafeReports.push(lineIdx);
-  }
+  // And put the original back in
+  LineArray.push(values);
+
+  LineArray.forEach((valSet) => {
+    const signs = [];
+    const diffs = [];
+    for (let valIdx = 0; valIdx < valSet.length - 1; valIdx += 1) {
+      const actualDiff = valSet[valIdx] - valSet[valIdx + 1];
+      const sign = Math.sign(actualDiff);
+      if (valIdx > 0 && sign != signs[0]) continue; // Changing signs!
+      signs.unshift(sign);
+      const posDif = Math.abs(actualDiff);
+      if (posDif < 1 || posDif > 3) continue; // First diff out of range?
+      diffs.unshift(posDif);
+    }
+    // We made it through the checks - this one is safe!
+    if (diffs.length === valSet.length - 1) {
+      SafeReports.push(lineIdx);
+    }
+  });
 }
 
-console.log(SafeReports.length);
+// SafeReports should have entries for each safe COMBINATION, let's sort for uniqueness
+const SafeSetSize = new Set(SafeReports).size;
+
+console.log(SafeSetSize);
